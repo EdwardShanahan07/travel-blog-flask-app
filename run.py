afd3c61f3a55e170a79c32511d63ccc978410733
Edward Shanahan <edwardshanahan357@gmail.com>
2023-04-10 19:08:25 +0100

    Add route to all html files

--- run.py
+++ run.py
@@ -6,7 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index(): 
-    return render_template("index.html")
+    return render_template("index.html", title="Welcome")
+
+
+@app.route("/about")
+def about(): 
+    return render_template("about.html", title="About")
+
+
+@app.route("/blog")
+def blog(): 
+    return render_template("blog.html", title="Blog")
+
+
+@app.route("/contact")
+def contact(): 
+    return render_template("contact.html", title="Contact")
 
 
 if __name__ == "__main__":
