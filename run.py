6390be348cf81ebd402d022ba959d390187b7c6f
Edward Shanahan <edwardshanahan357@gmail.com>
2023-04-10 18:50:50 +0100

    Add route to index.html file

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Flask App
 
 app = Flask(__name__)
+
+@app.route("/")
+def index(): 
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5001", debug=True)
